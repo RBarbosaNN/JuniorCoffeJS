9cf4f1fcf23b09bfb2332ab9d692b9c7f41a22c4
Rodrigo Barbosa <rodrigoalves-s@hotmail.com>
2021-10-22 01:38:18 -0300

    TestePC

---
+++ teste.js
@@ -0,0 +1,9 @@
+
+
+let olaMundo = 'Olá novo Mundo';
+
+function olaMundoNovo() {
+    console.log(olaMundo);
+    console.log('Arquivo gerado após trocar a porcaria do master para o Main');
+    console.log('Gerado no PC!');
+}
