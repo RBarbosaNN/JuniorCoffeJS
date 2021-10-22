bcb5737a5547dbf5f154b839c6a41c38b70a80cf
RBarbosaNN <rodrigoalves-s@hotmail.com>
2021-10-22 17:35:28 -0300

    add linha 9

--- teste.js
+++ teste.js
@@ -6,4 +6,5 @@ function olaMundoNovo() {
     console.log(olaMundo);
     console.log('Arquivo gerado após trocar a porcaria do master para o Main');
     console.log('Gerado no PC!');
+    console.log('Gerado no PC após instalação do GitHubDesktop!');
 }
