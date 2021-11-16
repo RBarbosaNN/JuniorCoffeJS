3c94b1f64898f095814a8bb45f1cb78fd5becb87
RBarbosaNN <rodrigoalves-s@hotmail.com>
2021-11-16 00:13:48 -0300

    PIM4
    Atualização do programa.
    Agora ele esta escrevendo em arquivo.txt

--- ExerciciosFileStream/escreveEmTxt.js
+++ ExerciciosFileStream/escreveEmTxt.js
@@ -1,9 +1,20 @@
 
-
-const fs = require('fs').promises;
-const path = require('path');
-const caminhoArquivo = path.resolve(__dirname, 'Teste.txt');
-
-fs.writeFile(caminhoArquivo, 'Texto 1\n', {flag: 'a'});
+const fs = require('fs');
 
 
+function escreveNoTxt (dados) {
+    
+    fs.appendFile('arquivo.txt', dados + '\n', (err) => {
+        if (err) throw err;
+       console.log('O arquivo foi criado!');
+    });
+
+}
+
+const dadosTwo = 'Teste número 2';
+const dadosTree = 'Teste número 3';
+const dadosFor = 'Teste número 4';
+
+escreveNoTxt(dadosTwo);
+escreveNoTxt(dadosTree);
+escreveNoTxt(dadosFor);
--- Pim4Inicio/cadastro.js
+++ Pim4Inicio/cadastro.js
@@ -1,47 +1,60 @@
 
+const fs = require('fs');
 
+    
+function formateParaTipoData(dataNascimentoStr) {
+    const data = new Date(dataNascimentoStr);
 
-function iniciaCadastro() {
-    
-    function formateParaTipoData(dataNascimentoStr) {
-        const data = new Date(dataNascimentoStr);
-    
-        if (isNaN(data)) {
-            console.log('O argmuento passado para a função não é uma instância de Date');
-            console.log('Por favor, declare a data no formato: AAAA/MM/DD');
-        } else {
-            const dia = addZeroAEsquerda(data.getDate());
-            const mes = addZeroAEsquerda(data.getMonth() + 1);
-            const ano = addZeroAEsquerda(data.getFullYear());
-            function addZeroAEsquerda(num) {
-                return num >= 10 ? num : `0${num}`;
-            }
-            console.log(`${dia}/${mes}/${ano}`);
+    if (isNaN(data)) {
+        console.log('O argmuento passado para a função não é uma instância de Date');
+        console.log('Por favor, declare a data no formato: AAAA/MM/DD');
+    } else {
+        const dia = addZeroAEsquerda(data.getDate());
+        const mes = addZeroAEsquerda(data.getMonth() + 1);
+        const ano = addZeroAEsquerda(data.getFullYear());
+        function addZeroAEsquerda(num) {
+            return num >= 10 ? num : `0${num}`;
         }
+        return (`${dia}/${mes}/${ano}`);
     }
-
 }
 
 
+
 class CadastroDePaciente {
+
     constructor(nomeStr, cpfStr, telStr, emailStr, enderecoStr, dataNascimentoStr, dataDiagnosticoStr, comorbidadesStr) {
-
+        escreveNoTxt('################################');
         this.nome = nomeStr;
+        escreveNoTxt(`Nome: ${nomeStr}`);
         this.cfp = cpfStr;
+        escreveNoTxt(`CPF: ${cpfStr}`);
         this.tel = telStr;
+        escreveNoTxt(`Telefone: ${telStr}`);
         this.email = emailStr;
+        escreveNoTxt(`E-mail: ${emailStr}`);
         this.endereco = enderecoStr;
-        this.dataNascimento = dataNascimentoStr;
-        this.dataDiagnostico = dataDiagnosticoStr;
+        escreveNoTxt(`Endereço: ${enderecoStr}`);
+        this.dataNascimento = escreveNoTxt(`Data de Nascimento: ${formateParaTipoData(dataNascimentoStr)}`);
+        this.dataDiagnostico = escreveNoTxt(`Data de Diagnostico: ${formateParaTipoData(dataDiagnosticoStr)}`);
         this.comorbidades = comorbidadesStr;
-
+        escreveNoTxt(`Comorbidades: ${comorbidadesStr}`);
+        escreveNoTxt('################################');
     }
 }
 
 const paciente1 = new CadastroDePaciente('Paulo', '345.787.899-22', '9 9899-2323', 'paulo@gmail.com', 'Rua 24 São Paulo', '1990/11/29', '2021/03/17', 'Gordão');
 
-console.log(paciente1);
 
+
+function escreveNoTxt (dados) {
+    
+    fs.appendFileSync('arquivo.txt', dados + '\n', (err) => {
+        if (err) throw err;
+       console.log('O arquivo foi criado!');
+    });
+
+}
 
 
 
