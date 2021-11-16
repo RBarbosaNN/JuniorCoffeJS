0e23d982392add4c98973a73d447300a01990070
RBarbosaNN <rodrigoalves-s@hotmail.com>
2021-11-16 20:55:49 -0300

    PIM4
    Ajustes no programa.
    
    Continuação do desenvolvimento do PIM 4, agora já existe interação com o usuário e os dados estão sendo gravados no arquivo.txt.

--- Pim4Inicio/cadastro.js
+++ Pim4Inicio/cadastro.js
@@ -14,10 +14,10 @@ function iniciaCadastro() {
     escreveNoTxt(`E-Mail: ${emailStr}`);
     let enderecoStr = entradaDeDadosPeloUsuario('Endereço: ');
     escreveNoTxt(`Endereço: ${enderecoStr}`);
-    let dataNascimentoStr = entradaDeDadosPeloUsuario('Data de Nascimento: ');
-    escreveNoTxt(`Data de Nascimento: ${dataNascimentoStr}`);
-    let dataDiagnosticoStr = entradaDeDadosPeloUsuario('Data do Diagnostico: ');
-    escreveNoTxt(`Data do diagnostico positivo para SARS-COV-2: ${dataDiagnosticoStr}`);
+    let dataNascimentoStr = entradaDeDadosPeloUsuario('Data de Nascimento no formato AAAA/MM/DD: ');
+    escreveNoTxt(`Data de Nascimento: ${formateParaTipoData(dataNascimentoStr)}`);
+    let dataDiagnosticoStr = entradaDeDadosPeloUsuario('Data do Diagnostico no formato AAAA/MM/DD: ');
+    escreveNoTxt(`Data do diagnostico positivo para SARS-COV-2: ${formateParaTipoData(dataDiagnosticoStr)}`);
     let comorbidadesStr = entradaDeDadosPeloUsuario('Paciente tem comorbidades? Qual?');
     escreveNoTxt(`Comorbidades: ${comorbidadesStr}`);
     escreveNoTxt('######################################################################');
@@ -39,38 +39,18 @@ function iniciaCadastro() {
         }
     }
 
-    // class CadastroDePaciente {
-    
-    //     constructor(nomeStr, cpfStr, telStr, emailStr, enderecoStr, dataNascimentoStr, dataDiagnosticoStr, comorbidadesStr) {
-    //         escreveNoTxt('################################');
-    //         this.nome = nomeStr;
-    //         escreveNoTxt(`Nome: ${nomeStr}`);
-    //         this.cfp = cpfStr;
-    //         escreveNoTxt(`CPF: ${cpfStr}`);
-    //         this.tel = telStr;
-    //         escreveNoTxt(`Telefone: ${telStr}`);
-    //         this.email = emailStr;
-    //         escreveNoTxt(`E-mail: ${emailStr}`);
-    //         this.endereco = enderecoStr;
-    //         escreveNoTxt(`Endereço: ${enderecoStr}`);
-    //         this.dataNascimento = escreveNoTxt(`Data de Nascimento: ${formateParaTipoData(dataNascimentoStr)}`);
-    //         this.dataDiagnostico = escreveNoTxt(`Data de Diagnostico: ${formateParaTipoData(dataDiagnosticoStr)}`);
-    //         this.comorbidades = comorbidadesStr;
-    //         escreveNoTxt(`Comorbidades: ${comorbidadesStr}`);
-    //         escreveNoTxt('################################');
-    //     }
-    // }
-
     function escreveNoTxt (dados) {
         
         fs.appendFileSync('arquivo.txt', dados + '\n', (err) => {
-            if (err) throw err;
-           console.log('O arquivo foi criado!');
+            if (err) {
+                throw err;
+            } else {
+                console.log('O arquivo foi criado!');
+            }
         });
     
     }
 
-    // const paciente1 = new CadastroDePaciente('Paulo', '345.787.899-22', '9 9899-2323', 'paulo@gmail.com', 'Rua 24 São Paulo', '1990/11/29', '2021/03/17', 'Gordão');
 }
     
 
