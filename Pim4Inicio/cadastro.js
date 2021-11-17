92a4e8375738a37a65a32d82d0b568c4040158d5
RBarbosaNN <rodrigoalves-s@hotmail.com>
2021-11-17 11:30:49 -0300

    Pim4
    Validação da entrada do tipo Data já esta ocorrendo.
    Preciso apenas impedir que o programa escreva "undefined" no txt
    toda vez que o usuario errar o formato da data

--- Pim4Inicio/cadastro.js
+++ Pim4Inicio/cadastro.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const entradaDeDadosPeloUsuario = require('prompt-sync')({sigint: true});
 
 function iniciaCadastro() {
+    let validaDN = false;
+    let validaDD = false;
 
     let nomeStr = entradaDeDadosPeloUsuario('Digite o nome completo do paciente: ');
     escreveNoTxt(`Nome: ${nomeStr}`);
@@ -14,20 +16,26 @@ function iniciaCadastro() {
     escreveNoTxt(`E-Mail: ${emailStr}`);
     let enderecoStr = entradaDeDadosPeloUsuario('Endereço: ');
     escreveNoTxt(`Endereço: ${enderecoStr}`);
-    let dataNascimentoStr = entradaDeDadosPeloUsuario('Data de Nascimento no formato AAAA/MM/DD: ');
-    escreveNoTxt(`Data de Nascimento: ${formateParaTipoData(dataNascimentoStr)}`);
-    let dataDiagnosticoStr = entradaDeDadosPeloUsuario('Data do Diagnostico no formato AAAA/MM/DD: ');
-    escreveNoTxt(`Data do diagnostico positivo para SARS-COV-2: ${formateParaTipoData(dataDiagnosticoStr)}`);
+    verificaDataNascimento()
+    verificaDataDiagnostico()
     let comorbidadesStr = entradaDeDadosPeloUsuario('Paciente tem comorbidades? Qual?');
     escreveNoTxt(`Comorbidades: ${comorbidadesStr}`);
     escreveNoTxt('######################################################################');
 
-    function formateParaTipoData(dataNascimentoStr) {
-        const data = new Date(dataNascimentoStr);
-    
+    function formateParaTipoDataN(pData) {
+        const data = new Date(pData);
         if (isNaN(data)) {
+            console.log('<*----------------------------------------------------------*>');
             console.log('O argmuento passado para a função não é uma instância de Date');
             console.log('Por favor, declare a data no formato: AAAA/MM/DD');
+            console.log('<*----------------------------------------------------------*>');
+
+            if(validaDN == false) {
+                verificaDataNascimento();
+            } else if (validaDN == true && validaDD == false) {
+                verificaDataDiagnostico();
+            }
+
         } else {
             const dia = addZeroAEsquerda(data.getDate());
             const mes = addZeroAEsquerda(data.getMonth() + 1);
@@ -35,6 +43,31 @@ function iniciaCadastro() {
             function addZeroAEsquerda(num) {
                 return num >= 10 ? num : `0${num}`;
             }
+            validaDN = true
+            return (`${dia}/${mes}/${ano}`);
+        }
+    }
+
+    function formateParaTipoDataD(pData) {
+        const data = new Date(pData);
+        if (isNaN(data)) {
+            console.log('<*----------------------------------------------------------*>');
+            console.log('O argmuento passado para a função não é uma instância de Date');
+            console.log('Por favor, declare a data no formato: AAAA/MM/DD');
+            console.log('<*----------------------------------------------------------*>');
+
+            if(validaDD == false) {
+                verificaDataDiagnostico()
+            }
+
+        } else {
+            const dia = addZeroAEsquerda(data.getDate());
+            const mes = addZeroAEsquerda(data.getMonth() + 1);
+            const ano = addZeroAEsquerda(data.getFullYear());
+            function addZeroAEsquerda(num) {
+                return num >= 10 ? num : `0${num}`;
+            }
+            validaDD = true;
             return (`${dia}/${mes}/${ano}`);
         }
     }
@@ -49,6 +82,20 @@ function iniciaCadastro() {
             }
         });
     
+    }
+
+    function verificaDataNascimento() {
+        let dataNascimentoStr = formateParaTipoDataN(entradaDeDadosPeloUsuario('Data de Nascimento no formato AAAA/MM/DD: '));
+        if (validaDN == true) {
+            escreveNoTxt(`Data de Nascimento: ${dataNascimentoStr}`);
+        }
+    }
+
+    function verificaDataDiagnostico() {
+        let dataDiagnosticoStr = formateParaTipoDataD(entradaDeDadosPeloUsuario('Data do Diagnostico no formato AAAA/MM/DD: '));
+        if (validaDD == true) {
+            escreveNoTxt(`Data do diagnostico positivo para SARS-COV-2: ${dataDiagnosticoStr}`);
+        }
     }
 
 }
