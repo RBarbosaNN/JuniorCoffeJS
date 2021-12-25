e30ea94ff17cdb5004a97bb598d7e18980d67d9b
RBarbosaNN <rodrigoalves-s@hotmail.com>
2021-12-25 01:14:55 -0300

    Update mega da virada
    Hoje 25/12/2021, o algoritmo evoluiu mais um pouco. Já esta fazendo a contagem de números repetidos, mas ainda precisa
        refinar mais. Esta apresentando as informações duplicadas no console.log()

--- MegaDaVirada/index.js
+++ MegaDaVirada/index.js
@@ -7,6 +7,7 @@ const nMinimo = 1;
 const nMaximo = 60;
 let contador = 0;
 let todosResultados = [];
+let listaDeRepetidos = [];
 
 function randomico(nMinimo, nMaximo) {
     const rdm = Math.random() * (nMaximo - nMinimo) + nMinimo;
@@ -19,48 +20,61 @@ function tornemeRico() {
     let limiteDeDezena = 6;
     let limiteDeJogo = 3;
 
-    for(let n = 0; n < limiteDeDezena; n++) {
+    for (let n = 0; n < limiteDeDezena; n++) {
         jogo.push(randomico(nMinimo, nMaximo));
     }
     contador += 1;
-    if(contador <= limiteDeJogo) {
+    if (contador <= limiteDeJogo) {
         console.log(`Jogo ${contador} = ${jogo}`);
-        todosResultados.push(jogo.splice(0));
+        for (n of jogo) {
+            todosResultados.push(n);
+        }
         tornemeRico();
     } else {
         console.log('Fim dos jogos!');
+        contador = 0;
         classificaRepetido();
     }
-
 }
 
 
 function classificaRepetido() {
     let contaRepetido = 0;
     let nParaComparar = 0;
-    console.log(`todosResultados = ${todosResultados}`);
-    for (let i of todosResultados) {
-        console.log(i);
-        nParaComparar = todosResultados[i];
-
-        for (let n = 0; n <= todosResultados.length - 1; n++) {
-
-            if (nParaComparar == todosResultados[n]) {
+    // console.log(`todosResultados = ${todosResultados}`);
+    for (i of todosResultados) {
+        nParaComparar = i;
+        for (c of todosResultados) {
+            if (nParaComparar == c) {
                 contaRepetido += 1;
-                console.log(`${nParaComparar} apareceu ${contaRepetido} vezes!`);
-            } else {
-                console.log(`${nParaComparar} não repetiu nenhuma vez.`);
+                registraListaDeRepetidos(nParaComparar, contaRepetido);
             }
-
         }
-
+        contaRepetido = 0;
     }
 }
+
+
+function registraListaDeRepetidos(nPc, cont) {
+    let recebeNpC = nPc;
+    let recebeCont = cont; 
+    if (recebeCont > 1) {
+        console.log(`${recebeNpC} repetiu ${recebeCont} vezes`);
+        recebeNpC = 0;
+        recebeCont = 0;
+    } else {
+        recebeNpC = 0;
+        recebeCont = 0;
+    }
+}
+
 
 tornemeRico();
 
 
 /*
     o algoritmo já traz os resultados, mas estou trabalhanho para criar uma função que mostra os numeros que 
-    mais se repetem. Ainda estou estudando isso!
+    mais se repetem. Ainda estou estudando isso! 23/12/2021
+    Hoje 25/12/2021, o algoritmo evoluiu mais um pouco. Já esta fazendo a contagem de números repetidos, mas ainda precisa
+    refinar mais. Esta apresentando as informações duplicadas no console.log()
  */
