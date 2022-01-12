b7cb817f9ae6d2460152f1fc4d27efe0d0d6ff53
RBarbosaNN <rodrigoalves-s@hotmail.com>
2022-01-12 11:11:57 -0300

    Update verificarFragIndice.sql

--- SQL/verificarFragIndice.sql
+++ SQL/verificarFragIndice.sql
@@ -8,3 +8,15 @@ select object_name(dm.object_id) AS Tabela,
 from sys.dm_db_index_physical_stats(db_id(),null,null,null,'sampled') dm
 join sys.indexes i on dm.object_id=i.object_id and dm.index_id=i.index_id
 order by avg_fragmentation_in_percent desc
+
+
+-- Essa query configura a utilização maxima de memória que a isntancia pode utilizar
+
+sp_configure 'show advanced options', 1;
+GO
+RECONFIGURE;
+GO
+sp_configure 'max server memory', 4096;
+GO
+RECONFIGURE;
+GO
