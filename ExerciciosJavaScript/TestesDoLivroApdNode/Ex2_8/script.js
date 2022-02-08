df7dcaf5eaef37610ce67e659c2346734c5c7ba2
RBarbosaNN <rodrigoalves-s@hotmail.com>
2022-02-07 22:10:31 -0300

    Update script.js

--- ExerciciosJavaScript/TestesDoLivroApdNode/Ex2_8/script.js
+++ ExerciciosJavaScript/TestesDoLivroApdNode/Ex2_8/script.js
@@ -1,6 +1,8 @@
 
 /*
-    Exemplo 2.8 - Uma aplicação sequencial síncrona
+    Exemplo 2.8 - Uma aplicação sequencial síncrona. Esse código pega o arquivo apples.txt que esta dentro da mesma pasta e procura
+    pela palavra Apple ou apple. Depois ele cria um novo arquivo arquivo com o nome: orange.txt e substitui todos as palavras que
+    ele encontrou por orange.
 */
 
 
