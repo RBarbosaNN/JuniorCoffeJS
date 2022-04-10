6834042da7e0d417cb2240473a9a5fbad9ee1d2a
RBarbosaNN <rodrigoalves-s@hotmail.com>
2022-04-09 21:52:05 -0300

    UpdateAulaBaltaIO

--- AulaBaltaIO-NodeAPI/node-str/bin/server.js
+++ AulaBaltaIO-NodeAPI/node-str/bin/server.js
@@ -1,28 +1,29 @@
-'use strict'
 
-const http = require('http');
+const app = require('../src/app');
 const debug = require('debug')('nodestr: server');
-const express = require('express');
+const http = require('http');
+// const express = require('express');
 
-const app = express();
+// const app = express();
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 const server = http.createServer(app);
-const router = express.Router();
 
-const route = router.get('/', (req, resp, next) => {
-    resp.status(200).send({
-        title: "Node Store API",
-        version: "0.0.1"
-    });
-});
+// const router = express.Router();
 
-app.use('/', route);
+// const route = router.get('/', (req, resp, next) => {
+//     resp.status(200).send({
+//         title: "Node Store API",
+//         version: "0.0.1"
+//     });
+// });
+
+// app.use('/', route);
 
 server.listen(port);
 server.on('error', onError);
-
+server.on('listening', onListening);
 console.log(`API rodando na porta: ${port}`);
 
 function normalizePort(val) {
@@ -60,3 +61,8 @@ function onError(error) {
     }
 }
 
+function onListening() {
+    const addr = server.address();
+    const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+}
