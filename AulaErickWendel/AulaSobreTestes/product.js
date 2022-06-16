f389c85636196d208b3554979ff6ec65030f5357
RBarbosaNN <rodrigoalves-s@hotmail.com>
2022-06-16 19:18:00 -0300

    Create product.js

---
+++ AulaErickWendel/AulaSobreTestes/product.js
@@ -0,0 +1,12 @@
+import {EventEmitter} from 'events'
+export default class Product {
+    constructor ({
+        onCreate,
+        service
+    }) {
+        this.service = service
+    }
+}
+
+//Parei no minuto 18:20 do curso:
+//https://www.youtube.com/watch?v=8hxhZkBzWhk
