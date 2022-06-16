fc8a9d342c516cd50722df65366389a334434074
RBarbosaNN <rodrigoalves-s@hotmail.com>
2022-06-16 20:57:30 -0300

    Update product.js

--- AulaErickWendel/AulaSobreTestes/product.js
+++ AulaErickWendel/AulaSobreTestes/product.js
@@ -5,7 +5,22 @@ export default class Product {
         service
     }) {
         this.service = service
+        this.source = new EventEmitter()
+        this.source.on('create', onCreate)
     }
+    //{ description, id, price }
+    #isValid(data) {
+        if(data.description.length < 5) {
+            throw new Error('description must be higher than 5')
+        }
+    }
+
+    async create(data) {
+        this.#isValid(data)
+        const message = await this.service.save(data)
+        return message.toUpperCase()
+    }
+
 }
 
 //Parei no minuto 18:20 do curso:
