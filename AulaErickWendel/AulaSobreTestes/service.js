baff62ec41123f344453e650e45090cdd284dd7b
RBarbosaNN <rodrigoalves-s@hotmail.com>
2022-06-16 21:05:43 -0300

    Delete service.js

--- AulaErickWendel/AulaSobreTestes/service.js
+++
@@ -1,8 +0,0 @@
-import { setTimeout } from "timers/promises";
-export default class Service {
-   async save(params) {
-        await setTimeout(2000)
-        return `${params.id} saved with success!`
-   }
-}
-
