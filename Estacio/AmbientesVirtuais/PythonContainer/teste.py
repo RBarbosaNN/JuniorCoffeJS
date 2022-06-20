145ef960ce63e2d153609a4dead6cd27f16cb709
RBarbosaNN <rodrigoalves-s@hotmail.com>
2022-06-20 12:36:14 -0300

    Update teste.py

--- Estacio/AmbientesVirtuais/PythonContainer/teste.py
+++ Estacio/AmbientesVirtuais/PythonContainer/teste.py
@@ -1,23 +1,6 @@
-from threading import Thread
 
-minha_lista = []
+import time
 
-def funcao():
-    for i in range(100000):
-        minha_lista.append(1)
-    for i in range(100000):
-        minha_lista.pop()
+X = time.time()
 
-
-if __name__ == '__main__':
-    tarefas = []
-    for indice in range(10):
-        tarefa = Thread(target=funcao)
-        tarefas.append(tarefa)
-        tarefas.append(tarefa)
-        tarefa.start()
-
-    print(len(minha_lista))
-    for tarefa in tarefas:
-        tarefa.join()
-        print(len(minha_lista))
+print(f'Hora local: {time.ctime(X)}')
