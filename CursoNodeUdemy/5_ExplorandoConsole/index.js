39be3c46a8d2f5a9e1a277fa1af0c2beb9a26a5f
RBarbosaNN <rodrigoalves-s@hotmail.com>
2022-06-27 23:32:34 -0300

    Create index.js

---
+++ CursoNodeUdemy/5_ExplorandoConsole/index.js
@@ -0,0 +1,17 @@
+const a1 = 10;
+const a2 = 'Kiko';
+const a3 = 101;
+const x1 = 11;
+
+//Imprimir mais de uma variável no mesmo console.
+console.log(a1, a2, a3)
+
+//Contar quantas vezes a variável x1 apareceu:
+console.count(`O valor de x1 = ${x1} e ela apareceu`);
+console.count(`O valor de x1 = ${x1} e ela apareceu`);
+console.count(`O valor de x1 = ${x1} e ela apareceu`);
+
+//Limpa o console. Para ver esse comando funcionando, o programa precisa ser executado via terminal
+setTimeout(() => {
+    console.clear()
+}, 2000)
