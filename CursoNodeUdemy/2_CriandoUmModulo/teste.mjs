8c792a52da924b8a33e72f1ac54d2def81625384
RBarbosaNN <rodrigoalves-s@hotmail.com>
2022-06-27 21:26:24 -0300

    Create teste.mjs

---
+++ CursoNodeUdemy/2_CriandoUmModulo/teste.mjs
@@ -0,0 +1,8 @@
+//Jeito novo de importar um módulo
+//Para rodar eu tive que ir pelo terminal.
+
+import somaDoisN from "./meu_novo_modulo.mjs";
+
+somaDoisN(1000,1000);
+somaDoisN(30, 20);
+somaDoisN(11, 11);
